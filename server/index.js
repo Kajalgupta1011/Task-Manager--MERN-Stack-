1210cf823d152f50cf03375a7f72ad66f640c0af
Kajalgupta1011 <guptakajal040917@gmail.com>
2023-09-11 12:02:03 +0530

    check response from api

--- server/index.js
+++ server/index.js
@@ -4,10 +4,14 @@ import express from 'express';
 import cors from 'cors';
 // const express = require('express');
 import Routes from './Routes/route.js';
+import bodyParser from 'body-parser';
 
 const app = express();
 app.use(cors())
 app.use('/', Routes);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
 const PORT = 8000;
 Connection();
 app.listen(PORT, () => {
